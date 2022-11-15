026bb2baf3341427d8542d60e4387cb29b757dce
Kacper Chmielewski <kchmielewski@tlen.pl>
2022-11-15 13:59:27 +0100

    TaskManager Rails --> CRUD works

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "tasks", to: "tasks#index"
-  get "tasks/:title", to: "tasks#show", as: :task
-  get "tasks/:new", to: "tasks#new"
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update", as: :update
+  delete "tasks/:id", to: "tasks#destroy"
 end
